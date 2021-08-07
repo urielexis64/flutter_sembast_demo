a96b5dabacb27d676c511c00ef983b55f6d10558
Uriel Aispuro <urielalexis64@gmail.com>
2021-08-06 19:50:02 -0600

    add, remove and sort users

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:sembast/sembast.dart';
 import 'package:sembast_demo/db/app_theme.dart';
+import 'package:sembast_demo/db/users_store.dart';
+import 'package:sembast_demo/modesl/user.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,6 +12,57 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<User> users = [];
+
+  _load() async {
+    Finder finder = Finder(sortOrders: [SortOrder('name'), SortOrder('age')]);
+    users = await UsersStore.instance.find(finder);
+    setState(() {});
+  }
+
+  _add() async {
+    final user = User.fake();
+    await UsersStore.instance.add(user);
+    users.add(user);
+    setState(() {});
+  }
+
+  _removeAll() async {
+    final removeCount = await UsersStore.instance.removeAll();
+    users.clear();
+    setState(() {});
+    ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('$removeCount items were removed.')));
+  }
+
+  _removeById(id) async {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Are you sure?'),
+        actions: [
+          TextButton(
+              onPressed: () async {
+                await UsersStore.instance.removeById(id);
+                users.removeWhere((user) => user.id == id);
+                setState(() {});
+                Navigator.pop(context);
+                ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('User removed successfully.')));
+              },
+              child: Text('Yes')),
+          TextButton(onPressed: () => Navigator.pop(context), child: Text('No'))
+        ],
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _load();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,18 +75,38 @@ class _HomePageState extends State<HomePage> {
               })
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [],
+      body: ListView.builder(
+        itemCount: users.length,
+        itemBuilder: (_, i) {
+          final user = users[i];
+          return ListTile(
+            onLongPress: () => _removeById(user.id),
+            title: Text(user.name),
+            subtitle: Text(user.email),
+            trailing: Text('${user.age}'),
+          );
+        },
       ),
       floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.end,
         children: [
           FloatingActionButton(
-            onPressed: () {},
+            onPressed: _load,
+            heroTag: 'reload',
+            child: Icon(
+              Icons.sort,
+              color: Colors.white,
+            ),
+            backgroundColor: Colors.lightGreen,
+          ),
+          SizedBox(
+            width: 15,
+          ),
+          FloatingActionButton(
+            onPressed: _removeAll,
             heroTag: 'clear',
             child: Icon(
-              Icons.clear,
+              Icons.clear_all,
               color: Colors.white,
             ),
             backgroundColor: Colors.redAccent,
@@ -41,7 +115,7 @@ class _HomePageState extends State<HomePage> {
             width: 15,
           ),
           FloatingActionButton(
-            onPressed: () {},
+            onPressed: _add,
             heroTag: 'add',
             child: Icon(
               Icons.person_add,
